cd1ca9e7003912538a20f4d0ba202ea41df8c3f6
yesrin <yerin.seo@mataedu.com>
2024-10-16 23:24:03 +0900

    [테스트코드] 좌석이용권과 사물함이용권의 타입과 기간이 같은지 확인한다.

--- src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassTest.java
+++ src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassTest.java
@@ -1,5 +1,6 @@
 package cleancode.studycafe.tobe.model.pass;
 
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,19 @@ class StudyCafeSeatPassTest {
         // Then
         assertEquals(1000, discountPrice);
 
+    }
+
+    @DisplayName("좌석이용권과 사물함 이용권의 타입과 기간이 같은지 확인한다.")
+    @Test
+    void isSameDurationType() {
+        // Given
+        StudyCafeSeatPass seatPass = StudyCafeSeatPass.of(StudyCafePassType.FIXED, 1, 10000, 0.1);
+        StudyCafeLockerPass lockerPass = StudyCafeLockerPass.of(StudyCafePassType.FIXED, 1, 2000);
+
+        // When
+        boolean result = seatPass.isSameDurationType(lockerPass);
+
+        // Then
+        assertTrue(result);
     }
 }
